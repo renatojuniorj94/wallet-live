f79def3dd548913dbcc2b6d805431effa3b591e1
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2026-08-23 22:08:00 -0300

    Creating server

--- project-wallet-live/src/main.rs
+++ project-wallet-live/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let router = Router::new().route("/", );
+    axum::serve(listener, router).await.unwrap();
 }
